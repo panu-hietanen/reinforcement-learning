0738303ece76c7b4d8cfa47dd3c7b2979505c45d
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2024-09-13 15:46:33 +0100

    Add useful funcitons

--- utils/Benchmarking.py
+++ utils/Benchmarking.py
@@ -1,3 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
+def mean_absolute_diff(y_pred: torch.Tensor, y_pred_noisy: torch.Tensor) -> float:
+    """Calculate mean absolute difference between y and yhat."""
+    return torch.mean(torch.abs(y_pred - y_pred_noisy)).item()
+
+
+def plot_noise_diff(td_sgd: float, td_adam: float, nn_sgd: float, nn_adam: float) -> None:
+    """Plot a bar chart of mean absolute difference."""
+    methods = ['TD-SGD', 'TD-Adam', 'NN-SGD', 'NN-Adam']
+    changes = [td_sgd, td_adam, nn_sgd, nn_adam]
+
+    plt.bar(methods, changes, color=['blue', 'green', 'red', 'orange'])
+    plt.ylabel('Average Change in Output')
+    plt.title('Comparison of Regression Methods with Gaussian Noise')
+    plt.show()
